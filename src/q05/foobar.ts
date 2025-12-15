f38c8c305d4b53a25a69911b3a16d17d3327dd93
paekung <supakit.3t@gmail.com>
2025-12-15 17:50:49 +0700

    q05

---
+++ src/q05/foobar.ts
@@ -0,0 +1,24 @@
+const myNum : number = Number(process.argv[2]);
+
+if(!myNum || !(myNum>=0)){
+    console.log("Invalid Input");
+    process.exit();
+}
+
+
+let result = '';
+for(let i = 1; i<=myNum;i++){
+    if(i%3==0 && i%7==0){
+        result+=`FooBar\n`;
+        continue
+    }else if(i%3 == 0){
+        result+="Foo";
+        continue
+    }else if(i%7 == 0){
+        result+="Bar";
+        continue
+    }
+    result+=`${i}\n`;
+}
+
+console.log(result)
