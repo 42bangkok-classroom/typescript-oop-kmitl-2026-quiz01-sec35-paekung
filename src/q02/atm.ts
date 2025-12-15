f64fa7bb148ad04a48273436280f868b286183c9
paekung <supakit.3t@gmail.com>
2025-12-15 09:34:25 +0700

    q02

---
+++ src/q02/atm.ts
@@ -0,0 +1,20 @@
+const Balance : number = Number(process.argv[2]);
+const Withdrawal : number = Number(process.argv[3]);
+
+if(!Balance || !Withdrawal || isNaN(Balance) || isNaN(Withdrawal)){
+    console.log("Invalid Input")
+    process.exit();
+}
+
+
+if(Withdrawal > 5000){
+    console.log("Exceeds per-withdrawal limit");
+}
+else if(Withdrawal > Balance){
+    console.log("Insufficient balance");
+}
+else if (Withdrawal < Balance){
+    console.log("Withdrawal approved");
+} else {
+    console.log("Invalid Input");
+}
