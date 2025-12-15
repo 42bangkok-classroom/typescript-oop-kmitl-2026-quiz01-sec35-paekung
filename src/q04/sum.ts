cdfc61fc5be22982ba5a4fd457c83d4d2a2a5c01
paekung <supakit.3t@gmail.com>
2025-12-15 17:27:58 +0700

    q04

---
+++ src/q04/sum.ts
@@ -0,0 +1,14 @@
+const myNum : number = parseInt(process.argv[2]);
+
+if(!myNum || myNum<=0){
+    console.log("Invalid Input");
+    process.exit();
+}
+
+let sum = 0;
+
+for(let i = 0;i<=myNum;i++){
+    sum+=i;
+}
+
+console.log(sum);
